dd582d0ae379fe625e320e85eaba8b67a5c6e3dd
Alejandro Gomez Pasade <alex.pasade@gmail.com>
2018-08-21 18:28:29 +0200

    Header y Footer renderizados y con sus estilos importados

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/footer.css'
+
+const Footer = () => (
+    <div className='footer'>
+        <h3>Alejandro Gómez Pasadé. Copyright 2018</h3>
+    </div>
+)
+
+export default Footer;
