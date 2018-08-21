0ce30b7fc8c483d993cb5aa6dece5b6032e42f6e
Alejandro Gomez Pasade <alex.pasade@gmail.com>
2018-08-21 18:28:29 +0200

    Header y Footer renderizados y con sus estilos importados
    Card actualizada

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/header.css'
+
+const Header = () => (
+    <div className='header'>
+        <h1 className='header__title' >Billing</h1>
+        <h2 className='header__subtitle' >Code Challenge</h2>
+    </div>
+)
+
+export default Header
