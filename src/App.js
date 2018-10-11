e23c434a91fd0b5c9d6c1f562b1bb4023cdfd6a7
Alejandro Gomez Pasade <alex.pasade@gmail.com>
2018-10-11 12:57:24 +0200

    fallos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import request from './request';
 import { ARTICLES_QUERY } from './queries';
-<<<<<<< HEAD
 import {BrowserRouter, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './styles/bases.css';
-=======
 import Header from './components/Header';
 import './styles/bases.css'
 import Footer from './components/Footer';
->>>>>>> 0ce30b7fc8c483d993cb5aa6dece5b6032e42f6e
 
 class App extends Component {
   // definition
@@ -33,15 +30,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
-<<<<<<< HEAD
-=======
         <h2>Billin code challenge</h2>
->>>>>>> 0ce30b7fc8c483d993cb5aa6dece5b6032e42f6e
         <pre>{JSON.stringify(this.state.articles, null, 2)}</pre>
         <Footer/>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,9 @@ const Footer = () => (
         <h3>Alejandro Gómez Pasadé. Copyright 2018</h3>
     </div>
 )
-
-<<<<<<< HEAD
 export default Footer;
 
 const Header = () => {
     
 }
-=======
 export default Footer;
->>>>>>> 0ce30b7fc8c483d993cb5aa6dece5b6032e42f6e
