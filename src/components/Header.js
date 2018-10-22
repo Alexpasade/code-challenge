174c3916f3c42fb9b4c88982455a382a49660ec7
Alejandro Gomez Pasade <alex.pasade@gmail.com>
2018-10-22 21:12:04 +0200

    borrado

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,12 @@ import '../styles/header.css'
 
 const Header = () => (
     <div className='header'>
-<<<<<<< HEAD
         <div className='container'>
             <h1 className='header__title' >Billing</h1>
             <h2 className='header__subtitle' >Code Challenge</h2>
         </div>
-=======
         <h1 className='header__title' >Billing</h1>
         <h2 className='header__subtitle' >Code Challenge</h2>
->>>>>>> 0ce30b7fc8c483d993cb5aa6dece5b6032e42f6e
     </div>
 )
 
